76fd62c21f78798f7619b92d103b6bec74ca1872
percevil <peeevil@gmail.com>
2019-11-16 13:10:13 -0800

    footer done

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,56 +10,52 @@
 			</div><!-- #content -->
 
 
-	<footer id="colophon" class="site-footer" 
-			role="contentinfo">
-
+	<footer id="colophon" class="site-footer" role="contentinfo">
 
 			<img class="wood-bkgd" src="<?php echo get_stylesheet_directory_uri();?>/images/dark-wood.png"/>
 
 				
-			
-			<div class="info-block">
-
-				<div class="footer-block">
 					<div class="contact">
 						<h3>CONTACT INFO</h3>
-						<a href="info@inhabitent.com"></a>
-						<p>778-456-7891</p>
+						<i class="fas fa-envelope"> </i>
+						<a href="">info@inhabitent.com</a><break>
+						<p>
+						<i id="phone" class="fas fa-phone-alt">778-456-7891 </i>
+						</p>
+						<p id="social">
+						<i class="fab fa-facebook-square"></i>
+						<i class="fab fa-twitter-square"></i>
+						<i class="fab fa-google-plus-square"></i>
+						</p>
+					
+						
+						
 					</div>
-				</div>
+			
 
-				<div class="footer-block">
+				
 					<div class="bus-hours">
 						<h3>BUSINESS HOURS</h3>
 						<p>Monday-Friday: 9am to 5pm</p>
 						<p>Saturday: 10am to 2pm</p>
 						<p>Sunday: Closed</p>
 					</div>
-				</div>
+			
 
-				</div class="footer-block">
-					<div class="tree">
-
-					<img src="https://tent.academy.red/wp-content/themes/inhabitent/images/inhabitent-logo-text.svg" alt="Inhabitent logo">
+				<div>
+					<img class="tree" src="https://tent.academy.red/wp-content/themes/inhabitent/images/inhabitent-logo-text.svg" alt="Inhabitent logo">
 					<!-- <a href=""><img class="tent-logo" src="<?php echo get_stylesheet_directory_uri();?>/images/ihabitent-logo-text-dark.svg"/></a>  -->
 			
 				</div>
-				
-				</div>
-
-			
-
-
-
-
-			
 					<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 					</div><!-- .site-info -->
-				</footer><!-- #colophon -->
-			</div><!-- #page -->
+					<div class="copyright">Copyright © 2019 Inhabitent</div>
+						
+	</footer><!-- #colophon -->			
+		
 		
 		<?php wp_footer(); ?>
-		</footer>
+	
 	</body>
 </html>
