912de2574837479292bc05abfd2161ae831d65db
percevil <peeevil@gmail.com>
2019-11-25 15:11:12 -0800

    save

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -6,11 +6,12 @@
  */
 
 get_header(); ?>
-	<div class="grid-container"> <!-- grid container -->
+	
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
  
+	
 	<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
@@ -20,30 +21,40 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 			
+			<div class="grid-container"> <!-- grid container -->
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+				$args = array(  
+				'post_type' => 'product',
+				'posts_per_page' => 16    
+				);
+				$loop = new WP_Query( $args ); 
+			?>
+			<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
 				<?php
 					get_template_part( 'template-parts/content' );
 				?>
 
 			<?php endwhile; ?>
+			
+			</div><!-- grid container end -->
 
 			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
 
 			<?php /* CHANGE 'post' IN THIS LINE TO 'product' to POPULATE THE PRODUCTS ON THE HOME PAGE */
-   			$args = array( 'post_type' => 'product','numberposts'=>"2", 'order' => 'ASC' );
-  			 $product_posts = get_posts( $args ); // returns an array of posts
+   			// $args = array( 'post_type' => 'product','numberposts'=>"2", 'order' => 'ASC' );
+  			//  $product_posts = get_posts( $args ); // returns an array of posts
 ?>
 
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?> 
  
-		</div><!-- grid container end -->
+		
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -5,39 +5,47 @@
  * @package RED_Starter_Theme
  */
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-		
-	
 
-	<?php wp_head(); ?>
-	</head>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-	<body <?php body_class(); ?>>
-		<div id="page" class="hfeed site">
-			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+    <link href="https://fonts.googleapis.com/css?family=Merriweather&display=swap" rel="stylesheet">
 
-			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding" >
-					<img src="<?php echo get_stylesheet_directory_uri();?>/images/logos/inhabitent-logo-tent.svg"/>
-					<a href="#" class="search-toggle" aria-hidden="true">
-					
-		</a>
-					
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
-				
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-				<i class="fa fa-search"></i>
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
-			</header><!-- #masthead -->
 
-			<div id="content" class="site-content">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <div id="page" class="hfeed site">
+        <a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+
+        <header id="masthead" class="site-header" role="banner">
+            <div class="site-branding">
+
+
+                <a href="http://localhost:8881/inhabitent/			front-page/">
+                    <img src="<?php echo get_stylesheet_directory_uri();?>/images/logos/inhabitent-logo-tent.svg" />
+                    <a href="#" class="search-toggle" aria-hidden="true">
+                    </a>
+
+                    <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                            rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                    <p class="site-description"><?php bloginfo( 'description' ); ?></p>
+            </div><!-- .site-branding -->
+
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <i class="fa fa-search"></i>
+                <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
+                <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+            </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
+
+        <div id="content" class="site-content">
