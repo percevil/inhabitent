775f14468586ea89b6be2fdcc6a39ab9a1125795
percevil <peeevil@gmail.com>
2019-11-27 15:49:00 -0800

    front page progress

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -11,6 +11,9 @@ get_header(); ?>
     <div class="home-image">
         <img src="wp-content/themes/inhabitent/images/home-hero.jpg" alt="home-image">
     </div> -->
+<div class="background-container">
+ <img class="hero-image" src="<?php echo get_template_directory_uri().'/images/home-hero.jpg'?>" alt="hero-image">
+ </div>
 
 
     <div class="round-tent-logo">
@@ -19,64 +22,34 @@ get_header(); ?>
     </div>
 </div>
 
-<div class="shop-stuff">
-    <h2>SHOP STUFF</h2>
-</div>
-
-<section class="product-container">
 
 
-    <div class="product-block">
-
-        <section class="i-image">
-            <img src="wp-content/themes/inhabitent/images/product-type-icons/do.svg" alt="Do">
-        </section>
-
-        <!-- <p class="blurb">Get back to nature with all the tools and toys you need to enjoy the great outdoors.
-        </p> -->
-        <p class="do-buttons">
-            <a class="do-button1" href="">Do Stuff</a>
-        </p>
+<section class="product-info container">
+    <div class="shop-stuff">
+        <h2>SHOP STUFF</h2>
     </div>
 
-    <div class="product-block">
-        <section class="i-image">
-            <img class="i-image" src="<?php echo get_template_directory_uri(); ?>/images/product-type-icons/eat.svg" alt="Eat">
-        </section>
-
-        <!-- <p class="blurb">Nothing beats food cooked over a fire. We have all you need for good camping eats.
-
-        </p> -->
-
-        <p class="do-buttons">
-            <a class="do-button2" href="">Eat Stuff</a>
-        </p>
+    <?php
+    $terms = get_terms( array(
+        'taxonomy' => 'product_type',
+        'hide_empty' => 0,
+    ) );
+    if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) :
+    ?>
+    <div class="product-type-blocks product-container">
+        <?php foreach ( $terms as $term ) : ?>
+            <div class="product-type-block-wrapper product-block">
+                <div class="i-image">
+                  <img src="<?php echo get_template_directory_uri() . '/images/product-type-icons/' . $term->slug; ?>.svg" alt="<?php echo $term->name; ?>" />
+                </div>
+                <p><?php echo $term->description; ?></p>
+                <p class="do-buttons"><a href="<?php echo get_term_link( $term ); ?>" class="btn"><?php echo $term->name; ?> Stuff</a></p>
+            </div>
+        <?php endforeach; ?>
     </div>
-
-    <div class="product-block">
-        <section class="i-image">
-            <img class="i-image"<?php echo get_template_directory_uri(); ?> src="wp-content/themes/inhabitent/images/product-type-icons/sleep.svg" alt="Sleep">
-        </section>
-        <p class="blurb">Get a good night's rest in the wild in a home away from home that travels well.
-        </p>
-        <p class="do-buttons">
-            <a class="do-button3" href="" class="">Sleep Stuff</a>
-        </p>
-    </div>
-
-    <div class="product-block">
-        <section class="i-image">
-            <img class="i-image"<?php echo get_template_directory_uri(); ?> src="wp-content/themes/inhabitent/images/product-type-icons/wear.svg" alt="Wear">
-        </section>
-        <!-- <p class="blurb">From flannel shirts to toques, look the part while roughing it in the great outdoors.
-        </p> -->
-
-        <p class="do-buttons">
-            <a class="do-button4" href="" class="">Wear Stuff</a>
-        </p>
-    </div>
-    <div class="post-block"></div>
+    <?php endif; ?>
 </section>
+
 
 <!--**** START OF FIRST LOOP FOR JOURNAL PICS AND ENTRIES ****-->
 
@@ -91,51 +64,40 @@ get_header(); ?>
 
 
 <section class="media-container">
-    <!-- second loop -->
-    <div class="journal-container">
-
-        <div class="post-info-container">
-            ?><?php
-					$args = array( 'post_type' => 'post','numberposts'=>"3", 'order' => 'ASC' );
-					$product_posts = get_posts( $args ); // returns an array of posts?>
-
-            <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-            <?php /* Content from your array of post results goes here */ ?>
-            <div class="journal-meta">
-                <h3 class="entry-titleul"></h3>
-
-            </div>
-            <?php the_title(); ?>
-        </div>
-
-        <div class="post-picture-container">
-            <?php the_post_thumbnail(); ?>
-        </div>
-
-
-
-
-        <?php endforeach; wp_reset_postdata(); ?>
-        <!-- **** END OF FIRST LOOP FOR JOURNAL PICS AND ENTRIES ****-->
-    </div>
-</section>
-<!-- **** START OF SECOND LOOP FOR ADVENTURE PICS AND ENTRIES ****-->
-<?php 
-		$args = array( 'post_type' => 'post','numberposts'=>"3", 'order' => 'ASC' );
-		$product_posts = get_posts( $args ); // returns an array of posts?>
-
-<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-<?php /* Content from your array of post results goes here */ ?>
-<?php the_title(); ?>
-<?php endforeach; wp_reset_postdata(); ?>
-
-<!-- **** END OF SECOND LOOP FOR ADVENTURE PICS AND ENTRIES ****-->
-
-
 <h2>Inhabitent Journal</h2>
-<h2>Latest Adventures</h2>
+  
+        <!-- second loop -->
+        <div class="journal-container">
 
-<div class="journal-entries">
+                <?php
+                        $args = array( 'post_type' => 'post','numberposts'=>"3", 'order' => 'ASC' );
+                        $product_posts = get_posts( $args ); // returns an array of posts?>
+
+                <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+                <?php /* Content from your array of post results goes here */ ?>
+                <article class="journal-entry">
+                    <div class="post-picture-container">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+    ------>                <div class="journal-meta"> <!-- CREATES BUTTON FOR ARTICLE -->
+                        <?php the_title(); ?>
+                        <a href="<?php echo get_the_permalink(); ?>">Read entry</a>   
+                    </div>
+                </article>
+
+            <?php endforeach; wp_reset_postdata(); ?>
+            <!-- **** END OF FIRST LOOP FOR JOURNAL PICS AND ENTRIES ****-->
+        </div>
+</section>
+
+
+
+
+<section class="adventure-container">
+    <h2>Latest Adventures</h2>
+</section>
+
+
 
 
 
